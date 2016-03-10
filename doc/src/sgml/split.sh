14482b26c8cc8f5e5b39c979c8da0cbacd9bea2f
Tatsuo Ishii <ishii@postgresql.org>
2016-03-10 15:31:27 +0900

    巨大ファイル func.sgml を5つにスクリプトで分割します。
    split.shが分割用のスクリプトで、func.sgmlをfunc[0-4].sgmlに分割します。
    前提条件として、func.sgmlに、
    <!-- split-func1-start -->
    のような分割用のマーカーが記録されている必要があり、分割用のマーカーは
    消してはいけません(そのマーカーを入れ込む変更はこのコミットに入っています)。
    
    また、filelist.sgmlが変更され、func.sgmlの代わりにfunc0.sgmlがincludeさ
    れるようになります(func.sgmlは、スクリプトを動かしても変更されません)。
    
    func.sgmlを変更したら、split.shを起動してfunc[0-4].sgmlを更新してリポジ
    トリに登録する必要があります。もちろん、必ずしも全部が更新されるとは限
    らないので、変更されたものだけ登録すれば良いでしょう。

---
+++ doc/src/sgml/split.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# This script splits func.sgml into 4 files: namely func[0-4].sgml.
+# func.sgml remains untouched. func0.sgml should be included by
+# postgres.sgml instead of func.sgml.
+# "<!-- split-func1-start --> and <!-- split-func1-end --> etc. must be in func.sgml
+# in order to get this script working.
+
+sed -e '/^<!-- split-func1-start -->$/,/^<!-- split-func4-end -->$/d' \
+    -e '/<!-- split-func0-end -->/i&func1;\n&func2;\n&func3;\n&func4' \
+    func.sgml > func0.sgml
+sed -n -e '/^<!-- split-func1-start -->$/,/^<!-- split-func1-end -->$/p' func.sgml > func1.sgml
+sed -n -e '/^<!-- split-func2-start -->$/,/^<!-- split-func2-end -->$/p' func.sgml > func2.sgml
+sed -n -e '/^<!-- split-func3-start -->$/,/^<!-- split-func3-end -->$/p' func.sgml > func3.sgml
+sed -n -e '/^<!-- split-func4-start -->$/,/^<!-- split-func4-end -->$/p' func.sgml > func4.sgml
+
