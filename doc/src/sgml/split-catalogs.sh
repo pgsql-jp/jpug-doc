9430d6879e187a68d2cd29a2ddf6329f923f06b7
Tatsuo Ishii <ishii@sraoss.co.jp>
2022-11-03 16:22:21 +0900

    catalogs.sgmlをPostgreSQL 15.0対応するための準備です。
    - マージミスで、catalogs.sgmlの"split-catalogs3"マークが消えていたのを復活
    - view関係がcatalogs.sgmlからsystem-views.sgmlに移動したことにより、分割ファイルのcatalogs4.sgmlが不要になったので削除
    - catalogs4.sgmlが削除されたのでそれに合わせてsplit-catalogs.sgmlを調整
    - PostgreSQL 15.0の変更に合わせてmerge-catalogs.shを調整

--- doc/src/sgml/split-catalogs.sh
+++ doc/src/sgml/split-catalogs.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# This script splits catalogs.sgml into 4 files: namely catalogs[0-4].sgml.
+# This script splits catalogs.sgml into 4 files: namely catalogs[0-3].sgml.
 # catalogs.sgml remains untouched. catalogs0.sgml should be included by
 # postgres.sgml instead of catalogs.sgml.
 # "<!-- split-catalogs1-start --> and <!-- split-catalogs1-end --> etc. must be in catalogs.sgml
@@ -8,9 +8,9 @@
 comment='
 <!-- 警告：このファイルは直接編集しないでください！
 1. catalogs.sgmlを編集したら、split-catalogs.shを起動します。
-2. するとcatalogs[0-4].sgmlが生成されます。
-3. catalogs.sgmlとともにcatalogs[0-4].sgmlのうち変更されたファイルをcommit/pushしてpull requestを作成してください。
-4. レビューはcatalogs[0-4].sgmlに対して行います。
+2. するとcatalogs[0-3].sgmlが生成されます。
+3. catalogs.sgmlとともにcatalogs[0-3].sgmlのうち変更されたファイルをcommit/pushしてpull requestを作成してください。
+4. レビューはcatalogs[0-3].sgmlに対して行います。
 5. 指摘された点があればcatalogs.sgmlに反映し、1に戻ります。
 6. catalogs.sgmlの変更がなければ、pull requestをマージして終了です。お疲れ様でした！
 -->
@@ -26,6 +26,4 @@ echo "$comment"|sed -e '/^$/d' > catalogs2.sgml
 sed -n -e '/^<!-- split-catalogs2-start -->$/,/^<!-- split-catalogs2-end -->$/p' catalogs.sgml >> catalogs2.sgml
 echo "$comment"|sed -e '/^$/d' > catalogs3.sgml
 sed -n -e '/^<!-- split-catalogs3-start -->$/,/^<!-- split-catalogs3-end -->$/p' catalogs.sgml >> catalogs3.sgml
-echo "$comment"|sed -e '/^$/d' > catalogs4.sgml
-sed -n -e '/^<!-- split-catalogs4-start -->$/,/^<!-- split-catalogs4-end -->$/p' catalogs.sgml >> catalogs4.sgml
 
